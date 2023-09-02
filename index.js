d9a81283778b63c4d986d8bbcc955b949433f881
Андрей Мазунин (Andrey Mazunin) <97140182+js-easy-school@users.noreply.github.com>
2023-09-02 15:04:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const yargs = require('yargs')
+const pkg = require('./package.json')
+const { addNote, removeNote, printNotes } = require('./notes.controller')
+
+yargs.version(pkg.version)
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new note to list',
+  builder: {
+    title: {
+      type: 'string',
+      describe: 'Note title',
+      demandOption: true
+    }
+  },
+  async handler(argv) {
+    const {title} = argv;
+    await addNote(title);
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove note by id',
+  builder: {
+    id: {
+      type: 'string',
+      describe: 'Note id',
+      demandOption: true
+    }
+  },
+  async handler(argv) {
+    const {id} = argv;
+    await removeNote(id);
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'Print all notes',
+  async handler() {
+    await printNotes()
+  }
+});
+
+yargs.parse()
